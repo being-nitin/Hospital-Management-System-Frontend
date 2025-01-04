7385759f80df6072dc3bf8a0c193d3698d8cf6cc
Aditya Kumar <adityapal1409@gmail.com>
2025-01-04 22:23:33 +0530

    04Jan 22:22 reusble buttons

--- src/components/reusableComponents/Buttons.tsx
+++ src/components/reusableComponents/Buttons.tsx
@@ -1,20 +1,22 @@
-import { Button } from "../../components/ui/button";
+import { Button } from "../ui/button";
 import { cn } from "../../lib/utils";
 import { ReactElement } from "react";
 
 interface FlexibleButtonProps {
-	text: string;
-	variant: "primary" | "secondary";
-	icon?: ReactElement;
-	onClick?: () => void;
+	text: string; //text to be displayed inside the button.
+	variant: "primary" | "secondary"; //variant of the button, determines its styling.
+	icon?: ReactElement; //optional icon to display inside the button.
+	onClick?: () => void; //optional onClick handler for button actions.
 }
 
+// styling classes for different button variants and default styles.
 const styleObj = {
 	primary: "bg-primary text-white",
 	secondary: "border-primary bg-secondary text-primary hover:bg-secondary",
-	default: "text-white rounded-xl min-w-32 py-5",
+	default: "text-white rounded-md w-full py-5",
 };
 
+// FlexibleButton component for rendering customizable buttons with optional icons.
 export const FlexibleButton = ({
 	text,
 	variant,
@@ -32,6 +34,7 @@ export const FlexibleButton = ({
 	);
 };
 
+// CircularIconButton component for rendering circular buttons with an icon.
 export const CircularIconButton = ({ icon }: { icon: ReactElement }) => {
 	return (
 		<Button className={cn("bg-primary w-10 h-10 text-white rounded-full")}>
